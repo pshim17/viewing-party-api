c70246012d20ed2961bf3ab86b112fccd982f6f6
Patrick Shim <patrick.shim91@yahoo.com>
2025-02-04 23:32:26 -0500

    fix routes for movie search by search term

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::MoviesController < ApplicationController
-  def index 
+  def index     
     conn = Faraday.new(url: "https://api.themoviedb.org/3")
-    
     response = conn.get("movie/top_rated") do |faraday|
       faraday.params["api_key"] = Rails.application.credentials.tmdb[:key]
     end
@@ -20,6 +19,39 @@ class Api::V1::MoviesController < ApplicationController
       render json: { data: top_rated_movies }, status: :ok
     else
       render json: { error: "Failed to fetch top 20 rated movies" }, status: :bad_gateway
+    end
+  end
+
+  def search 
+    require'pry';binding.pry
+    conn = Faraday.new(url: "https://api.themoviedb.org/3")
+    query = params[:query]
+
+    if query.blank?
+      return render json: { error: "Please provide a search term" }, status: :bad_request
+    end
+
+    response = conn.get("search/movie") do |faraday|
+      faraday.params["api_key"] = Rails.application.credentials.tmdb[:key]
+      faraday.params["query"] = query
+    end
+    
+    require'pry';binding.pry
+
+    if response.success?
+      searched_movies = JSON.parse(response.body)["results"].first(20).map do |movie|
+        {
+          "id": movie["id"],
+          "type": "movie",
+          "attributes": {
+            "title": movie["title"],
+            "vote_average": movie["vote_average"]
+          } 
+        }
+      end
+      render json: { data: searched_movies }, status: :ok
+    else
+      render json: { error: "Failed to fetch movies by search terms." }, status: :bad_gateway
     end
   end
 end
