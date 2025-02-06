037fb42a478399cb7f83b6e5f8fe68ef71514e56
Patrick Shim <patrick.shim91@yahoo.com>
2025-02-05 22:34:11 -0500

    feat: added sad paths error messages

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -1,5 +1,7 @@
 class ViewingParty < ApplicationRecord
   has_many :create_joined_user_viewing_parties
   has_many :users, through: :create_joined_user_viewing_parties
+
+  validates :name, :start_time, :end_time, :movie_id, :movie_title, presence: true
 end
 
