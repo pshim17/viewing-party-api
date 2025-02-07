7a25c758a76838e18d363e31ebe524b71f1ee3b7
Patrick Shim <patrick.shim91@yahoo.com>
2025-02-06 19:08:52 -0500

    Fix: deleted migration files with no file

--- db/migrate/20250206235632_rename_create_joined_user_viewing_parties_to_user_viewing_parties.rb
+++
@@ -1,5 +0,0 @@
-class RenameCreateJoinedUserViewingPartiesToUserViewingParties < ActiveRecord::Migration[7.1]
-  def change
-    rename_table :create_joined_user_viewing_parties, :user_viewing_parties
-  end
-end
