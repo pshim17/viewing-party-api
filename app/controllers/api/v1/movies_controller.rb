93bf56fd90d7d0bd8f0fa833a9498bc8d04f534b
Patrick Shim <patrick.shim91@yahoo.com>
2025-02-06 19:47:25 -0500

    refactor: get_movie_by_search_term

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,39 +1,13 @@
 class Api::V1::MoviesController < ApplicationController
   def index    
-    if response.success?
-      top_rated_movies = MovieGateway.get_top_rated_movies
-      render json: { data: top_rated_movies }, status: :ok
-    end
+    top_rated_movies = MovieGateway.get_top_rated_movies
+    render json: { data: top_rated_movies }, status: :ok
   end
 
   def search 
-    conn = Faraday.new(url: "https://api.themoviedb.org/3")
-    query = params[:query]
-
-    if query.blank?
-      return render json: { error: "Please provide a search term" }, status: :bad_request
-    end
-
-    response = conn.get("search/movie") do |faraday|
-      faraday.params["api_key"] = Rails.application.credentials.tmdb[:key]
-      faraday.params["query"] = query
-    end
-    
-    if response.success?
-      searched_movies = JSON.parse(response.body)["results"].first(20).map do |movie|
-        {
-          "id": movie["id"],
-          "type": "movie",
-          "attributes": {
-            "title": movie["title"],
-            "vote_average": movie["vote_average"]
-          } 
-        }
-      end
-      render json: { data: searched_movies }, status: :ok
-    else
-      render json: { error: "Failed to fetch movies by search terms." }, status: :bad_gateway
-    end
+    search_term = params[:query]
+    search_movies = MovieGateway.get_movie_by_search_term(search_term)
+    render json: { data: searched_movies }, status: :ok
   end
 end
 
--- app/gateways/movie_gateway.rb
+++ app/gateways/movie_gateway.rb
@@ -2,14 +2,31 @@ class MovieGateway
   def self.get_top_rated_movies
     response = conn.get("movie/top_rated") 
     JSON.parse(response.body)["results"].first(20).map do |movie|
-    {
-      "id": movie["id"],
-      "type": "movie",
-      "attributes": {
-        "title": movie["title"],
-        "vote_average": movie["vote_average"]
-      } 
-    }
+      {
+        "id": movie["id"],
+        "type": "movie",
+        "attributes": {
+          "title": movie["title"],
+          "vote_average": movie["vote_average"]
+        } 
+      }
+      end
+  end
+
+  def self.get_movie_by_search_term(search_term)
+    query = params[:query]
+    response = conn.get("search/movie") do |faraday| 
+      faraday.params["query"] = search_term
+    end
+    JSON.parse(response.body)["results"].first(20).map do |movie|
+      {
+        "id": movie["id"],
+        "type": "movie",
+        "attributes": {
+          "title": movie["title"],
+          "vote_average": movie["vote_average"]
+        } 
+      }  
     end
   end
 
